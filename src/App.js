1394d1551cb50579239e6952a9b201ba488ad59c
Oleksandr Fedotov <fedotov4815162342@gmail.com>
2018-07-22 00:43:10 +0200

    integrate redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Rows from './components/Layout/Rows';
 import Widget from './components/Content/Widget';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import globalState from './state/index';
+
+const store = createStore(globalState);
 
 class App extends Component {
     render() {
         return (
-            <Rows>
-                <Widget key="widget-1">1</Widget>
-                <Widget key="widget-2">2</Widget>
-                <Widget key="widget-3">3</Widget>
-            </Rows>
+            <Provider store={store}>
+                <Rows>
+                    <Widget key="widget-1">1</Widget>
+                    <Widget key="widget-2">2</Widget>
+                    <Widget key="widget-3">3</Widget>
+                </Rows>
+            </Provider>
         );
     }
 }
---
+++ src/state/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import navigation from './navigationReducer';
+
+export default combineReducers({
+    navigation
+});
